dba08b81950eb412e554939a815dfaed5ff04029
JenniferUgo <JENNIFERUGO.NN@GMAIL.COM>
2023-06-16 02:10:05 +0100

    update

--- Module3A_JavaScriptCore/task5(app).js
+++ Module3A_JavaScriptCore/task5(app).js
@@ -2,8 +2,9 @@
 const day = 'Friday'
 
 //2
- const position = 5
+const position = 5
 
- //3
- const output = 'Today is ' + day +'. '+'It is the '+ position+'th day of the week.'
- console.log(output)
+//3
+const output = 'Today is '+day+'. '+'It is the '+position+'th day of the week.'
+console.log(output)
+
