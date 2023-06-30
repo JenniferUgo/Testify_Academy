0b5536ccc9b2d3767a534a2888c7ceb61bd9a23e
JenniferUgo <JENNIFERUGO.NN@GMAIL.COM>
2023-06-30 09:34:15 +0100

    Task 11 and 12

---
+++ Module3A_JavaScriptCore/T11_JavaScript array for top 5 friends.js
@@ -0,0 +1,8 @@
+/* Steps: 
+Declare a variable, friends and assign it an array [ ]
+Add 5 elements in the array (Each element should be of type string. Remember to separate each element by a comma.)
+Log your friends array to the console and take a look. */
+
+const friends = ['Usher', 'Tank', 'Tyrese', 'Chris', 'Mario']
+
+console.log(friends)
---
+++ Module3A_JavaScriptCore/T12_choose based on index in array.js
@@ -0,0 +1,22 @@
+/*
+From your array of best created in the previous task, choose your best friend based on their index in the array. 
+
+
+Steps: 
+
+On the next line below your array of friends, Declare a variable, bestFriend and assign your best friend from the array of friends. (Use bracket notation)
+Log bestFriend to the console to ensure you chose the right friend.
+ */
+
+const friends = ['Usher', 'Tank', 'Tyrese', 'Chris', 'Mario']
+
+const bestFriend = friends[0]
+console.log(bestFriend)
+
+//To replace an index in the Array, use the following code
+
+// const friends = ['Usher', 'Tank', 'Tyrese', 'Chris', 'Mario']
+
+// const bestFriend = friends[2]
+// friends[2] = 'Ginuwine'
+// console.log(friends)
