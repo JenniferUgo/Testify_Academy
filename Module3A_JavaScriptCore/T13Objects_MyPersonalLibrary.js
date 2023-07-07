210ba70e22f9d1255482ac7a046cf0777293bd80
JenniferUgo <JENNIFERUGO.NN@GMAIL.COM>
2023-07-07 01:51:39 +0100

    13,14,15,16

---
+++ Module3A_JavaScriptCore/T13Objects_MyPersonalLibrary.js
@@ -0,0 +1,30 @@
+/*
+My Personal Library
+
+
+Task
+
+Using object literal, write a JavaScript object, books.
+
+The book object should take the following properties
+
+title (string)
+
+description (string)
+
+numberOfPages (number)
+
+authour (string)
+
+reading (boolean)
+*/
+
+
+const books = {
+    title: "Twilight",
+    description: "Romantic vampire fantasy story",
+    numberOfPages: 500,
+    author: "Stephanie Meyer",
+    reading: true
+}
+console.log(books['title'])
